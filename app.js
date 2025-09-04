a16ab1e4aa5e3b20007bee3842b1c5bc9f5b374d
rafafrd <bolekao03@gmail.com>
2025-09-04 11:23:01 -0300

    atividade 3

--- app.js
+++ app.js
@@ -12,6 +12,38 @@ const PORT = 2800;
   A rota deve executar a operação e retornar o resultado.  
  */
 
+app.use(express.json());
+
+app.get("/operacao/:tipo", (req, res) => {
+  const { tipo } = req.params;
+  const { numUm, numDois } = req.query;
+  let resultado;
+
+  const numero1 = parseFloat(numUm);
+  const numero2 = parseFloat(numDois);
+  if (isNaN(numero1) || isNaN(numero2)) {
+    return res.status(500).json({ message: "Parâmetros inválidos" });
+  }
+  switch (tipo) {
+    case "soma":
+      resultado = numero1 + numero2;
+      break;
+    case "subtracao":
+      resultado = numero1 - numero2;
+      break;
+    case "multiplicacao":
+      resultado = numero1 * numero2;
+      break;
+    case "divisao":
+      if (numero2 === 0) {
+        return res.status(500).json({ message: "Divisão por zero não é permitida" });
+      }
+      resultado = numero1 / numero2;
+      break;
+    };
+  res.json({ resultado });
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: "Pagina não encontrada" });
 });
