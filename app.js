af8034cd2ab6cab7289b82b627ed6c8c8672bd84
rafafrd <bolekao03@gmail.com>
2025-09-11 11:24:00 -0300

    atividade 5

--- app.js
+++ app.js
@@ -18,29 +18,27 @@ app.get("/saudacao/:nome", (req, res) => {
   const { nome } = req.params;
   const { hora } = req.query;
   let saudacao;
-// hora = Number(hora);
-
-// if (isNaN(hora)) {};
-
-// if (hora < 0 || hora > 23) {};
-
-// if (nome == '') {
-// digite um nome valido
-// };
-  if (hora >= 0 && hora < 12) {
-    saudacao = "Bom dia";
-  } else if (hora >= 12 && hora < 18) {
-    saudacao = "Boa tarde";
-  }
-  else if (hora >= 18 && hora <= 23) {
-    saudacao = "Boa noite";
-  }
-  else {
+  // hora = Number(hora);
+  if (isNaN(hora)) {
     return res.status(400).json({ message: "Hora inválida" });
-  }
-  res.status(200).json({ message: `${saudacao}, ${nome}!` });
-});
+  } else {
+    // if (hora < 0 || hora > 23) {};
 
+    // if (nome == '') {
+    // digite um nome valido
+    // };
+    if (hora >= 0 && hora < 12) {
+      saudacao = "Bom dia";
+    } else if (hora >= 12 && hora < 18) {
+      saudacao = "Boa tarde";
+    } else if (hora >= 18 && hora <= 23) {
+      saudacao = "Boa noite";
+    } else {
+      return res.status(400).json({ message: "Hora inválida" });
+    }
+    res.status(200).json({ message: `${saudacao}, ${nome}!` });
+  }
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: "Pagina não encontrada" });
