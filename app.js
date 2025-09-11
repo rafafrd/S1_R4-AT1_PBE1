05529ec32ed806acf1304636e5829bac5825dba7
rafafrd <bolekao03@gmail.com>
2025-09-11 05:04:52 -0300

    base atv 5

--- app.js
+++ app.js
@@ -14,6 +14,33 @@ const PORT = 2800;
 
 app.use(express.json());
 
+app.get("/saudacao/:nome", (req, res) => {
+  const { nome } = req.params;
+  const { hora } = req.query;
+  let saudacao;
+// hora = Number(hora);
+
+// if (isNaN(hora)) {};
+
+// if (hora < 0 || hora > 23) {};
+
+// if (nome == '') {
+// digite um nome valido
+// };
+  if (hora >= 0 && hora < 12) {
+    saudacao = "Bom dia";
+  } else if (hora >= 12 && hora < 18) {
+    saudacao = "Boa tarde";
+  }
+  else if (hora >= 18 && hora <= 23) {
+    saudacao = "Boa noite";
+  }
+  else {
+    return res.status(400).json({ message: "Hora inválida" });
+  }
+  res.status(200).json({ message: `${saudacao}, ${nome}!` });
+});
+
 
 app.use((req, res) => {
   res.status(404).json({ message: "Pagina não encontrada" });
